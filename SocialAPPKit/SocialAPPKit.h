a026587ac7f9ae878bb682e91dbbc3fae2c4bd10
ForC <1653904195@qq.com>
2018-04-19 11:54:56 +0800

    Creat : Project Work

---
+++ SocialAPPKit/SocialAPPKit.h
@@ -0,0 +1,19 @@
+//
+//  SocialAPPKit.h
+//  SocialAPPKit
+//
+//  Created by ForC on 2018/4/19.
+//  Copyright © 2018年 ForC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocialAPPKit.
+FOUNDATION_EXPORT double SocialAPPKitVersionNumber;
+
+//! Project version string for SocialAPPKit.
+FOUNDATION_EXPORT const unsigned char SocialAPPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialAPPKit/PublicHeader.h>
+
+
