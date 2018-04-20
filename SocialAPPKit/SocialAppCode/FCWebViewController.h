a23e2c1b086b8063cb709fa8f44163f327b3427a
ForC <1653904195@qq.com>
2018-04-20 17:40:38 +0800

    增加webview公共组件

---
+++ SocialAPPKit/SocialAppCode/FCWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCWebViewController.h
+//  SocialAPPKit
+//
+//  Created by ForC on 2018/4/20.
+//  Copyright © 2018年 ForC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCWebViewController : UIViewController
+
+@end
