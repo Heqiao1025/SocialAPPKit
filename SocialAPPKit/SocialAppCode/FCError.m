763facbca8eed2291fddb6cacad714b0c8889334
ForC <1653904195@qq.com>
2018-04-21 12:24:46 +0800

    错误码增加

---
+++ SocialAPPKit/SocialAppCode/FCError.m
@@ -0,0 +1,25 @@
+//
+//  FCError.m
+//  SocialAPPKit
+//
+//  Created by ForC on 2018/4/21.
+//  Copyright © 2018年 ForC. All rights reserved.
+//
+
+#import "FCError.h"
+
+@implementation FCError
+
++ (instancetype)errorWithMessage: (NSString *)message {
+    return [self errorWithCode:-1000 message:message];
+}
+
++ (instancetype)errorWithCode: (NSInteger)code message: (NSString *)message {
+    return [[self class] errorWithDomain:@"FCERROR" code:code userInfo:@{@"message":message}];
+}
+
+- (NSString *)message {
+    return self.userInfo[@"message"];
+}
+
+@end
