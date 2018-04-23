54c793dd8b9df5a120d0a4d7fcb375f9c2f70fe5
ForC <1653904195@qq.com>
2018-04-23 16:58:05 +0800

    删除不用的类

--- SocialAPPKit/SocialAppCore/BaseRequest/FCBaseRequest.m
+++ SocialAPPKit/SocialAppCore/BaseRequest/FCBaseRequest.m
@@ -9,7 +9,6 @@
 #import "FCBaseRequest.h"
 #import "NSData+FCData.h"
 #import "NSString+FCString.h"
-#import "NSURLResponse+FCNetRequest.h"
 
 @interface FCBaseRequest()
 
@@ -30,8 +29,9 @@
     request.HTTPMethod = [self HttpMethodString];
     [self configRequestHeader:request];
     request.HTTPBody = [self httpParamters];
+    __weak typeof(self) weakSelf = self;
     [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        if (error || ![response fc_requestSuccess]) {
+        if (error || ![weakSelf requestSuccess:response]) {
             FCError *customError = [FCError errorWithCode:0 message:error.userInfo[@"message"]];
             [callBack sendError:customError];
         } else {
@@ -45,6 +45,14 @@
 #pragma mark Private Profession
 - (BOOL)isAvailableURL {
     return self.requestURL.absoluteString.length ? YES : NO;
+}
+
+- (BOOL)requestSuccess: (NSURLResponse *)reponse {
+    if (![reponse isKindOfClass:[NSHTTPURLResponse class]]) {
+        return YES;
+    }
+    NSHTTPURLResponse *httpReponse = (NSHTTPURLResponse *)reponse;
+    return (httpReponse.statusCode >= 200 || httpReponse.statusCode < 300);
 }
 
 #pragma mark Private HttpConfig
