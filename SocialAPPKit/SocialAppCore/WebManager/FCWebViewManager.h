f952304a26c489a26381ea93e3a122a7538b5d06
ForC <1653904195@qq.com>
2018-04-23 16:05:55 +0800

    增加webView处理类

---
+++ SocialAPPKit/SocialAppCore/WebManager/FCWebViewManager.h
@@ -0,0 +1,13 @@
+//
+//  FCWebViewManager.h
+//  SocialAPPKit
+//
+//  Created by ForC on 2018/4/23.
+//  Copyright © 2018年 ForC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FCWebViewManager : NSObject
+
+@end
---
+++ SocialAPPKit/SocialAppCore/WebManager/FCWebViewManager.m
@@ -0,0 +1,13 @@
+//
+//  FCWebViewManager.m
+//  SocialAPPKit
+//
+//  Created by ForC on 2018/4/23.
+//  Copyright © 2018年 ForC. All rights reserved.
+//
+
+#import "FCWebViewManager.h"
+
+@implementation FCWebViewManager
+
+@end
